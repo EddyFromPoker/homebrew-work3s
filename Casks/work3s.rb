47d1baafac931fd95cec7f9f86ade601c5732f16
EddyFromPoker <eddyfrompoker@gmail.com>
2025-11-23 23:11:27 +0000

    Add SHA256 for v0.1.19

--- Casks/work3s.rb
+++ Casks/work3s.rb
@@ -1,6 +1,6 @@
 cask "work3s" do
   version "0.1.19"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  sha256 "5af3e7fd09a0ff1565b0f3665bced782e1ba3b8d268354751227679382b19ec4"
 
   url "https://github.com/EddyFromPoker/work3s-desktop/releases/download/v#{version}/Work3s_#{version}_aarch64.dmg"
   name "Work3s"
