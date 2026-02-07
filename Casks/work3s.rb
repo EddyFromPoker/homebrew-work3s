e9c72c2254443dd25d33805ad1c2fb5104f08368
Eddy Ferreira <79837100+EddyFromPoker@users.noreply.github.com>
2026-02-07 22:06:55 +0000

    Fix work3s cask: point back to work3s-desktop for v0.3.12

--- Casks/work3s.rb
+++ Casks/work3s.rb
@@ -2,10 +2,10 @@ cask "work3s" do
   version "0.3.12"
   sha256 "cb9464aa8d8aae644e287c0367a96bd56a2d9846c2c7fb253b1881fc0a93709d"
 
-  url "https://github.com/EddyFromPoker/work3s/releases/download/v#{version}/Work3s_#{version}_aarch64.dmg"
+  url "https://github.com/EddyFromPoker/work3s-desktop/releases/download/v#{version}/Work3s_#{version}_aarch64.dmg"
   name "Work3s"
   desc "Git worktree management for macOS"
-  homepage "https://github.com/EddyFromPoker/work3s"
+  homepage "https://github.com/EddyFromPoker/work3s-desktop"
 
   livecheck do
     url :url
